695d6321aef1c7c0d0131e6a658623ce10569b0a
Andreas Møller <andreasmoller@Andreass-MacBook-Pro-2.local>
2014-10-06 14:59:05 +0100

    updates

--- services/MapsService.js
+++ services/MapsService.js
@@ -1,19 +1,18 @@
-'use strict';
-var squel = require('squel'),
-  squel = require('squel'),
-  db = require('./mysql'),
-  E = require('express-http-errors'),
-  P = require('bluebird'),
-  moment = require('moment');
+// 'use strict';
+// var squel = require('squel'),
+//   db = require('./mysql'),
+//   E = require('express-http-errors'),
+//   P = require('bluebird'),
+//   moment = require('moment');
 
-exports.getMap = function (query) {
-  return P.resolve(query)
-    .then(exports._buildMapQuery)
-    .then(db.query);
-};
-exports._buildMapQuery = function (query) {
-  return squel.select()
-    .from('"maps"')
-    .where('time < now()')
-    .limit(1000);
-};
+// exports.getMap = function (query) {
+//   return P.resolve(query)
+//     .then(exports._buildMapQuery)
+//     .then(db.query);
+// };
+// exports._buildMapQuery = function (query) {
+//   return squel.select()
+//     .from('"maps"')
+//     .where('time < now()')
+//     .limit(1000);
+// };
