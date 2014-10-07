93c29662f1f2d8218372d18fa0ee60a6da605ccf
Andreas Møller <andreasmoller@Andreass-MacBook-Pro-2.local>
2014-10-07 17:50:13 +0100

    updates

--- services/mysql.js
+++ services/mysql.js
@@ -18,37 +18,45 @@
 // exports.connection = connection;
 'use strict';
 var mysql2 = require('mysql2');
+var config = require('config');
+var P = require('bluebird');
 var url = require("url");
 var SocksConnection = require('socksjs');
 
 var mysql_server_options = {
-  host: '173.194.241.222',
-  port: 3306
+  host: url.parse(config.mysql).hostname,
+  port: url.parse(config.mysql).port
 };
 
 var socks_options = {
-  host: 'proxy-54-75-227-164.proximo.io',
+  host: url.parse(config.proxy).hostname,
   port: 1080,
-  user: 'proxy',
-  pass: '50a144708638-4096-ae38-976124a5e15c'
+  user: url.parse(config.proxy).auth.split(':')[0],
+  pass: url.parse(config.proxy).auth.split(':')[1],
 };
 
 var socksConn = new SocksConnection(mysql_server_options, socks_options);
 
 var mysql_options =  {
   database: 'sentia',
-  user: 'root',
-  password: 'sqlaboutsafe',
+  user: url.parse(config.proxy).auth.split(':')[0],
+  password: url.parse(config.proxy).auth.split(':')[1],
   stream: socksConn
 };
 
-var mysqlConn = mysql2.createConnection(mysql_options);
-
-mysqlConn.query('SELECT * from maps limit 1', function(err, rows, fields) {
-  if (err) throw err;
-
-  console.log('Result: ', rows);
-
-  socksConn.dispose();
-  mysqlConn.end();
+var connection = mysql2.createConnection(mysql_options);
+exports.query = function (query) {
+  return new P(function (resolve, reject) {
+    connection.query(query, function(err, rows, fields) {
+    if (err) {
+      return reject(err);
+    }
+    return resolve(rows);
+  });
 });
+};
+exports.close = function () {
+  socksConn.dispose();
+  connection.end();
+};
+
