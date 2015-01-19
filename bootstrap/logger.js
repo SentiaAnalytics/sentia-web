06e967400c71fb9200c1389e2c411ee5599cae03
Andreas Møller <andreasmoller@ldn-172-16-122-111.we7.local>
2015-01-19 10:43:50 +0000

    updated logging rules

--- bootstrap/logger.js
+++ bootstrap/logger.js
@@ -1,5 +1,10 @@
 'use strict';
 var logger = require('bragi');
+
+if (process.env.NODE_ENV === 'prod') {
+  logger.log('config', 'running in prod mode');
+  logger.options.groupsDisabled = ['debug'];
+}
 module.exports = function () {
   logger.promise = function (tags) {
       return function (msg) {
