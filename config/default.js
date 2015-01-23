9b048ce57270ab44824f79ea562bc6c42c3ff11b
Andreas Møller <andreasmoller@ldn-172-16-122-111.we7.local>
2015-01-23 10:14:41 +0000

    updates

--- config/default.js
+++ config/default.js
@@ -1,5 +1,8 @@
-console.log(JSON.parse(process.env.GCLOUD_CREDENTIALS));
-
+console.log('**********************');
+console.log(process.env.MONGOLAB_URI);
+console.log(process.env.MYSQL_URL);
+console.log(process.env.REDISCLOUD_URL);
+console.log('**********************');
 module.exports = {
   "port": process.env.PORT || 3000,
   "mongo" : process.env.MONGOLAB_URI,
