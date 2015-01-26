ef13a9db52370b5254303afad289cfeaa792383d
Andreas Møller <andreasmoller@ldn-172-16-122-111.we7.local>
2015-01-26 17:13:59 +0000

    updates

---
+++ routes/stores/floorplans.js
@@ -0,0 +1,32 @@
+'use strict';
+var gcloudstorage = require('../../services/gcloudstorage.service');
+var HTTPError = require('node-http-error');
+var logger = require('bragi');
+var middleware = require('../../middleware');
+
+exports.get = {
+  url : '/stores/floorplans/:floorId',
+  handler: function (req, res, next) {
+    console.log('res.query');
+    console.log(res.query);
+    var stream = gcloudstorage.getReadStream(req.query.company, 'floorplans/' +req.params.floorId +  '/latest.jpg');
+    stream.pipe(res);
+    stream.on('error', function (err) {
+      next(err);
+    });
+  },
+  middleware : [middleware.company]
+};
+
+exports.create = {
+  url : '/stores/floorplans/:floorId',
+  handler: function (req, res, next) {
+    var stream = gcloudstorage.getWriteStream(req.query.company, 'floorplans/' +req.params.floorId +  '/latest.jpg');
+    req.pipe(stream)
+    .on('end', function () {
+      res.status(201).end();
+    })
+    .on('error', next);
+  },
+  middleware : [middleware.company]
+};
