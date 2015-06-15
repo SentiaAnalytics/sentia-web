f0a304cb63023f08f8fd3db7e2de76e9bb07e8dc
Andreas Møller <andreas.moller@gmail.com>
2015-06-16 00:10:28 +0100

    update

--- ci/setup_gcloud.sh
+++ ci/setup_gcloud.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #install
 echo $GCLOUD_CREDENTIALS >> gcloud.json
-sudo google-cloud-sdk/bin/gcloud auth activate-service-account --key-file gcloud.json
+sudo google-cloud-sdk/bin/gcloud auth activate-service-account --key-file gloud.json
 # sudo google-cloud-sdk/bin/gcloud config set project sentia-analytics
 # sudo google-cloud-sdk/bin/gcloud config set compute/zone europe-west1-b
 # sudo google-cloud-sdk/bin/gcloud config set container/cluster web-cluster
