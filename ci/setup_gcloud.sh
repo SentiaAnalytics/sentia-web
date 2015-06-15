d95e078004cb3309760e2896e11907b6b66f8fa5
Andreas Møller <andreas.moller@gmail.com>
2015-06-15 23:22:32 +0100

    update

--- ci/setup_gcloud.sh
+++ ci/setup_gcloud.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #install
+$GCLOUD_CREDENTIALS >> gcloud.json
 wget https://dl.google.com/dl/cloudsdk/release/google-cloud-sdk.zip
 unzip google-cloud-sdk.zip
 rm google-cloud-sdk.zip
@@ -9,12 +10,8 @@ sudo google-cloud-sdk/install.sh \
   --bash-completion=true \
   --rc-path=/.bashrc \
   --disable-installation-options
-
 alias gcloud=google-cloud-sdk/bin/gcloud
 sudo gcloud --quiet components update  preview  alpha  beta  app
-
-#config
-$GCLOUD_CREDENTIALS >> gcloud.json
 sudo gcloud -q auth activate-service-account --key-file gcloud.json
 sudo gcloud config set project sentia-analytics
 sudo gcloud config set compute/zone europe-west1-b
