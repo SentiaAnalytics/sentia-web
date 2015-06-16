0034929660708de6fb903d87169fa174bfb08af6
Andreas Møller <andreas.moller@gmail.com>
2015-06-16 09:40:34 +0100

    update

--- ci/run_docker.sh
+++ ci/run_docker.sh
@@ -4,4 +4,4 @@ docker run \
   -e MONGOLAB_URI=mongodb://heroku_app29196195:7cj4e5276l95m73ekpoqqgbm0i@ds063769.mongolab.com:63769/heroku_app29196195 \
   -e MYSQL_URL=mysql://root:sqlaboutsafe@173.194.254.87:3306/sentia \
   -e REDISCLOUD_URL=redis://rediscloud:S0yE1FbtKmsbElYP@pub-redis-16319.eu-west-1-1.2.ec2.garantiadata.com:16319 \
-  gcr.io/sentia_analytics/web:$CIRCLE_BUILD_NUM
+  cullophid/web:$CIRCLE_BUILD_NUM
