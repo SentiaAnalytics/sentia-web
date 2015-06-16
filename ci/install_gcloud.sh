f577b88e4711da842c98b4383c538c1033b596c4
Andreas Møller <andreas.moller@gmail.com>
2015-06-16 10:31:19 +0100

    update

--- ci/install_gcloud.sh
+++ ci/install_gcloud.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo apt-get update && sudo apt-get install -y -qq --no-install-recommends wget unzip python php5-mysql php5-cli php5-cgi openjdk-7-jre-headless openssh-client python-openssl && sudo apt-get clean
+sudo apt-get update && sudo apt-get install -y -qq --no-install-recommends wget unzip python openssh-client python-openssl && sudo apt-get clean
 wget https://dl.google.com/dl/cloudsdk/release/google-cloud-sdk.zip && unzip google-cloud-sdk.zip && rm google-cloud-sdk.zip
 google-cloud-sdk/install.sh --usage-reporting=true --path-update=true --bash-completion=true --rc-path=/.bashrc --disable-installation-options
 google-cloud-sdk/bin/gcloud --quiet components update pkg-go pkg-python pkg-java preview alpha beta app
