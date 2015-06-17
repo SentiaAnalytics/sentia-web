8ae4be3c11f382adb0235f91881d1cfa3233cc35
Andreas Møller <andreas.moller@gmail.com>
2015-06-17 01:16:18 +0100

    udpate

---
+++ ci/docker_run.sh
@@ -0,0 +1,8 @@
+docker run \
+  -d \
+  --log-driver=json-file \
+  -p 3000:3000 \
+  -e MONGOLAB_URI= $MONGOLAB_URI \
+  -e MYSQL_URL=$MYSQL_URL \
+  -e REDISCLOUD_URL=$REDISCLOUD_URL \
+  cullophid/sentia:$CIRCLE_BUILD_NUM >> dockerid.txt
