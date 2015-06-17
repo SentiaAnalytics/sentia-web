84508df98f426a30b1d0bf56af41d74ad8bdbc98
Andreas Møller <andreas.moller@gmail.com>
2015-06-17 01:36:31 +0100

    fixed docker_run

--- ci/docker_run.sh
+++ ci/docker_run.sh
@@ -1,7 +1,7 @@
 docker run \
   -d \
   -p 3000:3000 \
-  -e MONGOLAB_URI= $MONGOLAB_URI \
+  -e MONGOLAB_URI=$MONGOLAB_URI \
   -e MYSQL_URL=$MYSQL_URL \
   -e REDISCLOUD_URL=$REDISCLOUD_URL \
   cullophid/sentia:$CIRCLE_BUILD_NUM
