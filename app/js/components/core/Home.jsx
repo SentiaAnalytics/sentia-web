dad0cb6f6add156ec25cf04b7df96a7d7b728dfa
Andreas Møller <andreas.moller@gmail.com>
2015-06-20 11:56:29 +0100

    update

--- app/js/components/core/Home.jsx
+++ app/js/components/core/Home.jsx
@@ -34,7 +34,7 @@ export default React.createClass({
   render: function () {
     return (
       <div className="bg-gray-ligther">
-        <h1>v2.0.23</h1>
+        <h1>v2.0.24</h1>
         <RouteHandler session={this.state.session} />
       </div>
     );
