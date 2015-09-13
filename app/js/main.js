4499c21c2a460f05dbd3c49cd3aa562e002600f0
Andreas Møller <andreas.moller@gmail.com>
2015-09-13 22:22:01 +0100

    test 3

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,5 @@
 'use strict';
-document.getElementById('test').style.background = 'red';
-// console.error('google', google);
-// import './globals';
-// import * as Router from './components/Router.jsx';
-// Router.init();
+error.stuff
+import './globals';
+import * as Router from './components/Router.jsx';
+Router.init();
