ff3ba457b016761b451ac72ea70f9bc7963752c5
Andreas Møller <andreas.moller@gmail.com>
2015-11-11 21:11:30 +0000

    update

--- services/redis.service.js
+++ services/redis.service.js
@@ -4,5 +4,7 @@ var redis = require('redis'),
   url = require("url").parse(config.redis),
   client = require("redis").createClient(url.port, url.hostname);
 
+console.log('REDIS', config.redis);
+
 client.auth(url.auth.split(":")[1]);
 module.exports = client;
